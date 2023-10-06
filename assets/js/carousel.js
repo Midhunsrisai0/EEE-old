62ce9f619d83abe7aa259abd92b93009e485a926
Midhunsrisai0 <21A81A0624@sves.org.in>
2023-10-06 14:30:46 +0530

    kept library images carousel in comments

---
+++ assets/js/carousel.js
@@ -0,0 +1,18 @@
+// const slide = document.querySelector('.carousel-slide');
+// const prevBtn = document.querySelector('.prev-btn');
+// const nextBtn = document.querySelector('.next-btn');
+
+// let counter = 0;
+// const slideWidth = document.querySelector('.carousel-container').offsetWidth;
+
+// nextBtn.addEventListener('click', () => {
+//     if (counter >= 3) return;
+//     counter++;
+//     slide.style.transform = `translateX(${-slideWidth * counter}px)`;
+// });
+
+// prevBtn.addEventListener('click', () => {
+//     if (counter <= 0) return;
+//     counter--;
+//     slide.style.transform = `translateX(${-slideWidth * counter}px)`;
+// });
